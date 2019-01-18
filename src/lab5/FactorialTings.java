79b58711a56323b949ac4d4caa1ac69798b91cbd
Cynthia <cturner@ltu.edu>
2019-01-18 00:21:29 -0500

    modified loops

--- src/lab5/FactorialTings.java
+++ src/lab5/FactorialTings.java
@@ -1,10 +1,55 @@
 package lab5;
 
+import java.util.Scanner;
+
 public class FactorialTings {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scnr = new Scanner(System.in);
+		
+		String input = "y";
+		
+		while (input.equalsIgnoreCase("y")){ 
+		
+			System.out.println("Welcome to the Factorial Calculator!");
+			System.out.println("Enter an integer (1-10): ");
+			int num = scnr.nextInt();
+		
+		
+			
+	private static int answer(int num) {
+		
+			int answer = factorial(num);
+			System.out.println(answer);
+	
+	private static int factorial(int num) {
+			if (num == 1) {
+				return 1;
+			}else {
+				return num * factorial(num - 1);
+			
+			
+				
+			
+		
+		
+		
+//		String mood = "y";
+//		
+//		System.out.println("Continue?: (y/n)");//Allows user to exit program
+//		mood = scnr.next();
+//	    
+//		
+//		System.out.println("We are DONE here...");
+		
+			scnr.close();
+		
+			}
 	}
+	}
+		}
+	}	
+	}
+	
 
-}
